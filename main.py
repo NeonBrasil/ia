aa65a4180926792d9fedb413c20bbe30017c42f5
Cayque Cicarelli <cayquecicarelli@gmail.com>
2025-02-14 22:35:46 -0300

    Big update
    main: adicionado um loop quando o usuário perder no easter egg continuar o questionário
    intents: novas perguntas e respostas
    Código está estável

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 from chatbot import ChatBot
 import os
 import random
-import curses
+
+# Importar windows-curses se estiver no Windows
+try:
+    import curses
+except ImportError:
+    import windows_curses as curses
 
 myChatBot = ChatBot()
 
@@ -21,28 +26,29 @@ def start_snake_game():
     w.keypad(1)
     w.timeout(100)
 
-    snk_x = sw//4
-    snk_y = sh//2
+    snk_x = sw // 4
+    snk_y = sh // 2
     snake = [
         [snk_y, snk_x],
-        [snk_y, snk_x-1],
-        [snk_y, snk_x-2]
+        [snk_y, snk_x - 1],
+        [snk_y, snk_x - 2]
     ]
 
-    food = [sh//2, sw//2]
+    food = [sh // 2, sw // 2]
     w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
 
     key = curses.KEY_RIGHT
+    score = 0
 
     while True:
         next_key = w.getch()
         key = key if next_key == -1 else next_key
 
         if snake[0][0] in [0, sh] or \
-            snake[0][1] in [0, sw] or \
-            snake[0] in snake[1:]:
+                snake[0][1] in [0, sw] or \
+                snake[0] in snake[1:]:
             curses.endwin()
-            quit()
+            return score
 
         new_head = [snake[0][0], snake[0][1]]
 
@@ -58,11 +64,12 @@ def start_snake_game():
         snake.insert(0, new_head)
 
         if snake[0] == food:
+            score += 1
             food = None
             while food is None:
                 nf = [
-                    random.randint(1, sh-1),
-                    random.randint(1, sw-1)
+                    random.randint(1, sh - 1),
+                    random.randint(1, sw - 1)
                 ]
                 food = nf if nf not in snake else None
             w.addch(food[0], food[1], curses.ACS_PI)
@@ -74,19 +81,25 @@ def start_snake_game():
 
 print("Bem vindo ao Chatbot")
 
-pergunta = input("\ncomo posso te ajudar?")
-if pergunta.lower() == "easter egg":
-    start_snake_game()
-else:
-    resposta, intencao = myChatBot.chatbot_response(pergunta)
-    print(resposta + "   ["+intencao[0]['intent']+"]")
-
-    while (intencao[0]['intent'] != "despedida"):
-        pergunta = input("posso lhe ajudar com algo a mais?")
-        if pergunta.lower() == "easter egg":
-            start_snake_game()
-            break
+while True:
+    pergunta = input("\ncomo posso te ajudar?")
+    if pergunta.lower() == "easter egg":
+        score = start_snake_game()
+        print(f"Você conseguiu {score} pontos! Parabéns por achar o easter egg, posso te ajudar com algo a mais?")
+    else:
         resposta, intencao = myChatBot.chatbot_response(pergunta)
         print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+        while (intencao[0]['intent'] != "despedida"):
+            pergunta = input("posso lhe ajudar com algo a mais?")
+            if pergunta.lower() == "easter egg":
+                score = start_snake_game()
+                print(f"Você conseguiu {score} pontos! Parabéns por achar o easter egg, posso te ajudar com algo a mais?")
+            else:
+                resposta, intencao = myChatBot.chatbot_response(pergunta)
+                print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+    if intencao[0]['intent'] == "despedida":
+        break
 
 print("Foi um prazer atendê-lo")
